93acfc2cf86b2499db126265e7ef50364a293c6b
kishanhari54 <kishanhari54@gmail.com>
2022-08-29 14:31:35 +0530

    3 files modified, 2 added

--- src/app/Reusable/Accordian/accordian.component.ts
+++ src/app/Reusable/Accordian/accordian.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ContentChild, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'rs-accord',
-  templateUrl: './accordian.component.html'
+  templateUrl: './accordian.component.html',
+  styleUrls: ['./accordian.style.css'],
 })
-export class AccordianComponent  {
+export class AccordianComponent {
   name = 'Angular';
+  @Input() ContentData: any;
+  @ContentChild('title', { read: TemplateRef }) title: TemplateRef<any>;
+  @ContentChild('content', { read: TemplateRef }) content: TemplateRef<any>;
+
+  expand: boolean = false;
+  ngOnInit() {
+    console.log(this.ContentData);
+  }
+
+  toggler() {
+    this.expand = !this.expand;
+  }
 }
